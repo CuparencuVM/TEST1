d61a937ad89de84dbf419d444e84b9a5e3166042
cuparencuvm <cuparencuvm@gmail.com>
2019-10-05 13:13:01 +0300

    ISBN

---
+++ src/Java/Isbn.java
@@ -0,0 +1,36 @@
+package ISBN;
+
+import sun.awt.SunHints;
+
+public class Isbn {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean Isbn (String numbers) {
+
+        int i = numbers.length();
+                if ( i != 10)
+                    return false;
+
+        numbers = numbers.replaceAll("-", ""); // inlocuim liniutele cu nimic
+        for ( int i = 0 ; i < numbers.length() -1 ; i ++) {
+            char a = numbers.charAt(i);
+            int sum = 0;
+         //   sum = Character.getNumericValue()
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+}
